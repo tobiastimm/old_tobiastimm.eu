0067c8929b430309add98f862bb8e0fec8dacff2
Tobias Timm <tbs.timm@gmail.com>
2017-10-18 22:15:55 +0200

    fixed markdown renderin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,43 @@
+const _ = require("lodash");
 const path = require("path");
 const Promise = require("bluebird");
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
-  const contentSite = path.resolve(`src/templates/content-site.js`);
-
-  return graphql(`
-    {
-      site {
-        siteMetadata {
-          siteUrl
-          title
-          description
-        }
-      }
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            excerpt(pruneLength: 250)
-            html
-            id
-            frontmatter {
-              title
-              path
+  return new Promise((resolve, reject) => {
+    const pages = [];
+    const contentSite = path.resolve(`src/templates/content-site.js`);
+    resolve(
+      graphql(`
+      {
+        allMarkdownRemark(limit: 1000) {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
             }
           }
         }
       }
-    }
-  `).then(result => {
-    if (result.errors) {
-      console.log(result.errors);
-      return Promise.reject(result.errors);
-    }
+    `)
+        .then(result => {
+          if (result.errors) {
+            console.log(result.errors);
+            return Promise.reject(result.errors);
+          }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: contentSite,
-        context: {} // additional data can be passed via context
-      });
-    });
-  });
+          _.each(result.data.allMarkdownRemark.edges, edge => {
+            createPage({
+              path: edge.node.frontmatter.path,
+              component: contentSite,
+              context: {
+                path: edge.node.frontmatter.path,
+              },
+            })
+          })
+        })
+    );
+  })
 };
--- src/templates/content-site.js
+++ src/templates/content-site.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Helmet from "react-helmet";
+import styled from 'styled-components';
 
-export default function Template({ data, pathContext }) {
+const ContentSite = ({ data }) => {
   const { markdownRemark: post } = data; // data.markdownRemark holds our post data
   return (
     <div className="container-fluid">
       <div className="content-site-container">
         <Helmet title={`${post.frontmatter.title}`} />
         <div className="row">
-          <h1>{post.frontmatter.title}</h1>
+          <h2>{post.frontmatter.title}</h2>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
       </div>
@@ -16,22 +17,22 @@ export default function Template({ data, pathContext }) {
   );
 }
 
+export default ContentSite;
+
 export const pageQuery = graphql`
-  query ContentSiteByPath($refPath: String!) {
-    site {
-      siteMetadata {
-        title
-        description
-        siteUrl
-      }
-    }
-    markdownRemark(frontmatter: { path: { eq: $refPath } }) {
-      html
-      excerpt
-      frontmatter {
-        path
-        title
-      }
+query ContentSiteByPath($path: String!) {
+  site {
+    siteMetadata {
+      title
+      author
     }
   }
-`;
+  markdownRemark(frontmatter: { path: { eq: $path } }) {
+    id
+    html
+    frontmatter {
+      title
+    }
+  }
+}
+`
